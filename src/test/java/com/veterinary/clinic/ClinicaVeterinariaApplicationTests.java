6d9ca8c122236dc10bddfaf6ec6d06213fbe4339
Leon Vino <yleonv@migraciones.gob.pe>
2026-03-27 17:27:35 -0500

    Primer commit - proyecto clinica veterinaria

--- src/test/java/com/veterinary/clinic/ClinicaVeterinariaApplicationTests.java
+++ src/test/java/com/veterinary/clinic/ClinicaVeterinariaApplicationTests.java
@@ -2,8 +2,10 @@ package com.veterinary.clinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 👈 ESTA LÍNEA SOLUCIONA TODO
 class ClinicaVeterinariaApplicationTests {
 
     @Test
